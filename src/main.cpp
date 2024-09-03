b1f03378aaaf585c2471e2859b467a0721cb66d9
James Clark <james@citcs.org>
2024-09-03 10:06:03 +0100

    packet filter seems to be set

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 
 #include <Wire.h>
 
+#include <WiFi.h>
+
+
+#include <esp_wifi.h>
 
 
 // super secret codez!
@@ -47,6 +51,7 @@ void screenUpdate();
 #define LED_FLASHSTYLE_STROBE 0
 #define LED_FLASHSTYLE_HALF 1
 #define LED_FLASHSTYLE_SLOW 2
+#define LED_FLASHSTYLE_OFF 3
 
 
 /// @brief Set the flashing style of the led.
@@ -61,6 +66,25 @@ unsigned long ledTimeOff = 950;
 
 /// @brief Handle the led toggling
 void ledUpdate();
+
+
+
+
+
+/// @brief Execute the sniffer setup state machine.
+void doStateMachine();
+
+/// @brief The sniffer state machine current state.
+uint8_t snifferCurrentState = 0;
+
+
+/// @brief Reset the wifi chip to its default state.
+void wifiReset();
+
+
+void wifipromiscousmode();
+
+void sniffer(void* buf, wifi_promiscuous_pkt_type_t type);
 
 
 
@@ -86,6 +110,8 @@ void setup() {
   // configure the led pin
   pinMode( LED_BUILTIN , OUTPUT );
   digitalWrite( LED_BUILTIN , LOW );
+  // turn off the led
+  ledSet( LED_FLASHSTYLE_OFF );
 
 
   // configure i2c for buit in screen
@@ -111,8 +137,7 @@ void setup() {
   
 
 
-
-
+  
 
 }
 
@@ -151,10 +176,103 @@ void loop() {
   // check for screen update.
   screenUpdate();
 
+  // poll the sniffer state machine
+  doStateMachine();
+
+}
+
+
+
+
+
+
+
+
+/// @brief The number of networks found during the wifi scan.
+int8_t networksfound = 0;
+
+/// @brief The correct channel number for the target ssid.
+uint8_t channelNumber = 0;
+
+void doStateMachine() {
+
+
+  switch (snifferCurrentState)
+  {
+  case 0:
+    //initial startup.
+    Serial.println( "Wifi reset.");
+    wifiReset();
+    networksfound = 0;
+    snifferCurrentState = 1;
+    break;
+
+  case 1:
+    // begin scan for SSID
+    Serial.println("Scan started!");
+    WiFi.scanNetworks( true );
+    snifferCurrentState = 2;
+    break;
+
+  case 2:
+    networksfound = WiFi.scanComplete();
+    if ( networksfound < 0 ) {
+      return;
+    }
+    snifferCurrentState = 3;
+    break;
+
+  case 3:
+    Serial.print( "Scan complete: " );
+    Serial.print( networksfound );
+    Serial.println( " networks found." );
+    
+    for ( uint8_t i = 0 ; i < networksfound ; i++ ) {
+      Serial.print("Checking: " );
+      Serial.println( WiFi.SSID(i).c_str() );
+      if ( (String)WiFi.SSID(i).c_str() == WIFI_SSID ) {
+        Serial.print("Found ");
+        Serial.print( WIFI_SSID );
+        Serial.print( " on channel " );
+        Serial.println( WiFi.channel(i) );
+        channelNumber = WiFi.channel(i);
+      }
+    }
+
+    snifferCurrentState = 4;
+    break;
+
+  case 4:
+    // channel number is found
+    Serial.print("Channel Found: " );
+    Serial.print( channelNumber );
+    Serial.println();
+    // remove the scan results
+    WiFi.scanDelete();
+    // reset the wifi.
+    wifiReset();
+    snifferCurrentState = 5;
+    break;
+
+  case 5:
+    // configure wifi as promiscous.
+    Serial.println("Entering Promiscous Mode.");
+    wifipromiscousmode();
+    snifferCurrentState = 6;
+    break;
+
+  case 6:
+    break;
+
+  default:
+    break;
+  }
 
 
 
 }
+
+
 
 
 
@@ -203,6 +321,7 @@ void ledUpdate() {
 
 
 
+
 void ledSet( uint8_t ledFlashStyle ) {
 
   switch (ledFlashStyle) {
@@ -221,6 +340,11 @@ void ledSet( uint8_t ledFlashStyle ) {
       ledTimeOn = 750;
       ledTimeOff = 250;
       break;
+    
+    case LED_FLASHSTYLE_OFF:
+      ledTimeOn = 0;
+      ledTimeOff = 1000;
+      break;
 
   }
 
@@ -236,7 +360,7 @@ void ledSet( uint8_t ledFlashStyle ) {
 
 
 
-
+uint8_t guiActivityIndicator = 0;
 
 
 void screenUpdate() {
@@ -258,7 +382,16 @@ void screenUpdate() {
   display.clearDisplay();
 
   display.setCursor( 0 , 0 );
-  display.print( currentTime );
+  display.print("Ready");
+
+  if ( guiActivityIndicator ) {
+    display.print("!");
+    guiActivityIndicator = 0;
+  }
+  else {
+    guiActivityIndicator = 1;
+  }
+
 
   // update the display from the framebuffer.
   display.display();
@@ -282,6 +415,69 @@ void screenUpdate() {
 
 
 
+
+
+
+void wifiReset() {
+
+  // reset the wifi chip
+  WiFi.disconnect();
+
+  // set it to station mode
+  WiFi.mode( WIFI_STA );
+
+  delay(100);
+  
+}
+
+
+
+
+void wifipromiscousmode() {
+  wifi_promiscuous_filter_t filter = {
+		.filter_mask = WIFI_PROMIS_FILTER_MASK_ALL};
+	ESP_ERROR_CHECK(esp_wifi_set_promiscuous_filter(&filter));
+    esp_wifi_set_promiscuous(true);
+    esp_wifi_set_promiscuous_rx_cb(&sniffer);
+}
+
+
+
+
+// Callback function to process received packets
+void sniffer(void* buf, wifi_promiscuous_pkt_type_t type) {
+    wifi_promiscuous_pkt_t *pkt = (wifi_promiscuous_pkt_t *)buf;
+    wifi_pkt_rx_ctrl_t rx_ctrl = pkt->rx_ctrl;
+
+    // skip boradcast
+  if ( pkt->payload[ 4 + 0 ] == 0xFF) {
+    return;
+  }
+
+    Serial.print("Packet received: ");
+    Serial.print("RSSI: ");
+    Serial.print(rx_ctrl.rssi);
+    Serial.print(", Channel: ");
+    Serial.print(rx_ctrl.channel);
+    Serial.print(", Length: ");
+    Serial.print(rx_ctrl.sig_len);
+    Serial.print(", Type: ");
+    Serial.println(type);
+
+    // Print source and destination MAC addresses
+    Serial.print("Source MAC: ");
+    for (int i = 0; i < 6; i++) {
+        Serial.printf("%02x", pkt->payload[10 + i]);
+        if (i < 5) Serial.print(":");
+    }
+    Serial.print(", Destination MAC: ");
+    for (int i = 0; i < 6; i++) {
+        Serial.printf("%02x", pkt->payload[4 + i]);
+        if (i < 5) Serial.print(":");
+    }
+    Serial.println();
+
+}
 
 
 
